29a1e4df5609f951b689a597cc9b6c425634e776
Volifter <4546202-volifter@users.noreply.gitlab.com>
2025-12-08 09:50:17 +0200

    Add `Box3D.delta` and `Box3D.manhattanDelta`, implement `Box3D.getMinDistanceTo()` and `Box3D.fromPoints()`

--- src/utils/Box3D.kt
+++ src/utils/Box3D.kt
@@ -1,5 +1,9 @@
 package utils
 
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 data class Box3D(val start: Coords3D, val end: Coords3D) {
     val isEmpty = end.x < start.x || end.y < start.y || end.z < start.z
 
@@ -10,6 +14,18 @@ data class Box3D(val start: Coords3D, val end: Coords3D) {
     val yRange get() = (start.y..end.y)
 
     val zRange get() = (start.z..end.z)
+
+    val delta get() = sqrt(
+        ((end.x - start.x) * 1f).pow(2f)
+        + ((end.y - start.y) * 1f).pow(2f)
+        + ((end.z - start.z) * 1f).pow(2f)
+    )
+
+    val manhattanDelta: Int get() = (
+        (end.x - start.x).absoluteValue
+        + (end.y - start.y).absoluteValue
+        + (end.z - start.z).absoluteValue
+    )
 
     fun intersect(other: Box3D): Box3D =
         Box3D(
@@ -24,6 +40,15 @@ data class Box3D(val start: Coords3D, val end: Coords3D) {
                 minOf(end.z, other.end.z)
             )
         )
+
+    fun getMinDistanceTo(other: Box3D) = sqrt((0..2).fold(0f) { acc, i ->
+        val (left, right) = if (start[i] < other.start[i])
+            this to other
+        else
+            other to this
+
+        acc + maxOf(0f, (right.start[i] - left.end[i]) * 1f).pow(2)
+    })
 
     operator fun plus(offset: Coords3D) =
         Box3D(start + offset, end + offset)
@@ -43,6 +68,20 @@ data class Box3D(val start: Coords3D, val end: Coords3D) {
                     maxOf(cornerA.x, cornerB.x),
                     maxOf(cornerA.y, cornerB.y),
                     maxOf(cornerA.z, cornerB.z)
+                )
+            )
+
+        fun fromPoints(points: List<Coords3D>): Box3D =
+            Box3D(
+                Coords3D(
+                    points.minOf { it.x },
+                    points.minOf { it.y },
+                    points.minOf { it.z }
+                ),
+                Coords3D(
+                    points.maxOf { it.x },
+                    points.maxOf { it.y },
+                    points.maxOf { it.z }
                 )
             )
     }
